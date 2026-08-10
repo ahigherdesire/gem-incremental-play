aa239fb610fd53405747d31247960ca429b6e53a
lankystovegaming-debug <lankystovegaming@gmail.com>
2026-08-10 16:38:29 +0800

    initial gem data

---
+++ src/data/gems.js
@@ -0,0 +1,190 @@
+const gems = [
+  {
+    name: "Quartz",
+    rarity: 2,
+    baseWeight: 100,
+    valuePerGram: 0.05
+  },
+  {
+    name: "Calcite",
+    rarity: 3,
+    baseWeight: 110,
+    valuePerGram: 0.064
+  },
+  {
+    name: "Feldspar",
+    rarity: 5,
+    baseWeight: 125,
+    valuePerGram: 0.08
+  },
+  {
+    name: "Fluorite",
+    rarity: 8,
+    baseWeight: 140,
+    valuePerGram: 0.1
+  },
+  {
+    name: "Hematite",
+    rarity: 12,
+    baseWeight: 160,
+    valuePerGram: 0.119
+  },
+  {
+    name: "Obsidian",
+    rarity: 18,
+    baseWeight: 180,
+    valuePerGram: 0.139
+  },
+  {
+    name: "Agate",
+    rarity: 25,
+    baseWeight: 200,
+    valuePerGram: 0.16
+  },
+  {
+    name: "Jasper",
+    rarity: 35,
+    baseWeight: 225,
+    valuePerGram: 0.182
+  },
+  {
+    name: "Amethyst",
+    rarity: 50,
+    baseWeight: 250,
+    valuePerGram: 0.22
+  },
+  {
+    name: "Garnet",
+    rarity: 70,
+    baseWeight: 275,
+    valuePerGram: 0.262
+  },
+  {
+    name: "Peridot",
+    rarity: 100,
+    baseWeight: 300,
+    valuePerGram: 0.317
+  },
+  {
+    name: "Topaz",
+    rarity: 150,
+    baseWeight: 325,
+    valuePerGram: 0.415
+  },
+  {
+    name: "Aquamarine",
+    rarity: 225,
+    baseWeight: 350,
+    valuePerGram: 0.529
+  },
+  {
+    name: "Tourmaline",
+    rarity: 325,
+    baseWeight: 375,
+    valuePerGram: 0.667
+  },
+  {
+    name: "Opal",
+    rarity: 475,
+    baseWeight: 400,
+    valuePerGram: 0.9
+  },
+  {
+    name: "Zircon",
+    rarity: 650,
+    baseWeight: 425,
+    valuePerGram: 1.106
+  },
+  {
+    name: "Spinel",
+    rarity: 850,
+    baseWeight: 450,
+    valuePerGram: 1.389
+  },
+  {
+    name: "Sapphire",
+    rarity: 1100,
+    baseWeight: 475,
+    valuePerGram: 1.789
+  },
+  {
+    name: "Ruby",
+    rarity: 1400,
+    baseWeight: 500,
+    valuePerGram: 2.2
+  },
+  {
+    name: "Emerald",
+    rarity: 1800,
+    baseWeight: 525,
+    valuePerGram: 2.667
+  },
+  {
+    name: "Diamond",
+    rarity: 2300,
+    baseWeight: 550,
+    valuePerGram: 3.364
+  },
+  {
+    name: "Tanzanite",
+    rarity: 2900,
+    baseWeight: 575,
+    valuePerGram: 3.565
+  },
+  {
+    name: "Alexandrite",
+    rarity: 3600,
+    baseWeight: 600,
+    valuePerGram: 4.417
+  },
+  {
+    name: "Benitoite",
+    rarity: 4400,
+    baseWeight: 625,
+    valuePerGram: 4.8
+  },
+  {
+    name: "Red Beryl",
+    rarity: 5300,
+    baseWeight: 650,
+    valuePerGram: 5.538
+  },
+  {
+    name: "Black Opal",
+    rarity: 6300,
+    baseWeight: 675,
+    valuePerGram: 6.37
+  },
+  {
+    name: "Grandidierite",
+    rarity: 7400,
+    baseWeight: 700,
+    valuePerGram: 6.857
+  },
+  {
+    name: "Taaffeite",
+    rarity: 8500,
+    baseWeight: 725,
+    valuePerGram: 7.586
+  },
+  {
+    name: "Musgravite",
+    rarity: 9300,
+    baseWeight: 750,
+    valuePerGram: 8
+  },
+  {
+    name: "Painite",
+    rarity: 10000,
+    baseWeight: 800,
+    valuePerGram: 8.125
+  },
+  {
+    name: "Dark Matter",
+    rarity: 1000000,
+    baseWeight: 2500,
+    valuePerGram: 200
+  }
+];
+
+export default gems;
