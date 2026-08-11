f9293b850beaa0e39ec334628d9981f81c151036
lankystovegaming-debug <lankystovegaming@gmail.com>
2026-08-11 12:29:07 +0800

    added t2-5 pickaxes

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -38,6 +38,191 @@ const recipes = [
         luck: 0.05
       }
     }
+  },
+
+  {
+    id: "reinforced-pickaxe",
+    name: "Reinforced Pickaxe",
+    category: "pickaxe",
+
+    requirements: [
+      {
+        type: "equipment",
+        equipmentId: "crude-pickaxe"
+      },
+      {
+        type: "gem-count",
+        gem: "Hematite",
+        amount: 4
+      },
+      {
+        type: "gem-count",
+        gem: "Obsidian",
+        amount: 3
+      },
+      {
+        type: "gem-count",
+        gem: "Garnet",
+        amount: 2
+      },
+      {
+        type: "gem-count",
+        gem: "Peridot",
+        amount: 1
+      }
+    ],
+
+    moneyCost: 1000,
+
+    reward: {
+      id: "reinforced-pickaxe",
+      name: "Reinforced Pickaxe",
+      category: "pickaxe",
+
+      bonus: {
+        luck: 0.15
+      }
+    }
+  },
+
+  {
+    id: "polished-pickaxe",
+    name: "Polished Pickaxe",
+    category: "pickaxe",
+
+    requirements: [
+      {
+        type: "equipment",
+        equipmentId: "reinforced-pickaxe"
+      },
+      {
+        type: "gem-count",
+        gem: "Garnet",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Peridot",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Topaz",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Aquamarine",
+        amount: 1
+      }
+    ],
+
+    moneyCost: 3500,
+
+    reward: {
+      id: "polished-pickaxe",
+      name: "Polished Pickaxe",
+      category: "pickaxe",
+
+      bonus: {
+        luck: 0.50
+      }
+    }
+  },
+
+  {
+    id: "refined-pickaxe",
+    name: "Refined Pickaxe",
+    category: "pickaxe",
+
+    requirements: [
+      {
+        type: "equipment",
+        equipmentId: "polished-pickaxe"
+      },
+      {
+        type: "gem-count",
+        gem: "Topaz",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Aquamarine",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Tourmaline",
+        amount: 1
+      },
+      {
+        type: "gem-count",
+        gem: "Opal",
+        amount: 1
+      }
+    ],
+
+    moneyCost: 7500,
+
+    reward: {
+      id: "refined-pickaxe",
+      name: "Refined Pickaxe",
+      category: "pickaxe",
+
+      bonus: {
+        luck: 0.80
+      }
+    }
+  },
+
+  {
+    id: "masterwork-pickaxe",
+    name: "Masterwork Pickaxe",
+    category: "pickaxe",
+
+    requirements: [
+      {
+        type: "equipment",
+        equipmentId: "refined-pickaxe"
+      },
+      {
+        type: "gem-count",
+        gem: "Quartz",
+        amount: 100
+      },
+      {
+        type: "gem-count",
+        gem: "Feldspar",
+        amount: 50
+      },
+      {
+        type: "gem-count",
+        gem: "Hematite",
+        amount: 25
+      },
+      {
+        type: "gem-count",
+        gem: "Obsidian",
+        amount: 15
+      },
+      {
+        type: "gem-count",
+        gem: "Sapphire",
+        amount: 1
+      }
+    ],
+
+    moneyCost: 20000,
+
+    reward: {
+      id: "masterwork-pickaxe",
+      name: "Masterwork Pickaxe",
+      category: "pickaxe",
+
+      bonus: {
+        luck: 1.50
+      }
+    }
   }
 ];
 
