d957145f324a6410e67dd573d8f3912816f454c1
lankystovegaming-debug <lankystovegaming@gmail.com>
2026-08-11 17:06:25 +0800

    backend

---
+++ src/backend/supabase.js
@@ -0,0 +1,15 @@
+import {
+  createClient
+} from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL =
+  "https://fuzidbblwzrhhbonjqjm.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_sCiiZxMP5DG8_gjGLg9qUg_HnWVD27U";
+
+export const supabase =
+  createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+  );
